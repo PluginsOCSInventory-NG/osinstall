79b624b4cc400761b0e80f9d08becb22f22952c3
Stéphane <steph78630@gmail.com>
2017-08-02 23:06:28 +0200

    Add files via upload

--- APACHE/Map.pm
+++ APACHE/Map.pm
@@ -1,1 +1,29 @@
+###############################################################################
+## OCSINVENTORY-NG
+## Copyleft Stephane PAUTREL (2017)
+## Web : http://www.acb78.com
+##
+## This code is open source and may be copied and modified as long as the source
+## code is always made freely available.
+## Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
+################################################################################
+ 
+package Apache::Ocsinventory::Plugins::Osinstall::Map;
+ 
+use strict;
+ 
+use Apache::Ocsinventory::Map;
 
+$DATA_MAP{osinstall} = {
+	mask => 0,
+		multi => 1,
+		auto => 1,
+		delOnReplace => 1,
+		sortBy => 'INSTDATE',
+		writeDiff => 0,
+		cache => 0,
+		fields => {
+            INSTDATE => {}
+			}
+};
+1;
