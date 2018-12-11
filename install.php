bfe43b4bb32f6c27ff38738e253cfd52472591b0
Charlene Auger <charlene.auger@factorfx.com>
2018-12-11 09:43:05 +0100

    Update plugin for extension engine

--- install.php
+++ install.php
@@ -1,42 +1,39 @@
 <?php
-function plugin_version_osinstall()
+
+/**
+ * This function is called on installation and is used to create database schema for the plugin
+ */
+function extension_install_osinstall()
 {
-return array('name' => 'osinstall',
-'version' => '1.0',
-'author'=> 'Stephane PAUTREL',
-'license' => 'GPLv2',
-'verMinOcs' => '2.2');
+    $commonObject = new ExtensionCommon;
+
+    $commonObject -> sqlQuery("CREATE TABLE IF NOT EXISTS `osinstall` (
+                              `ID` INT(11) NOT NULL AUTO_INCREMENT,
+                              `HARDWARE_ID` INT(11) NOT NULL,
+                              `INSTDATE` VARCHAR(255) DEFAULT NULL,
+                              `BUILDVER` VARCHAR(255) DEFAULT NULL,
+                              `CODESET` VARCHAR(255) DEFAULT NULL,
+                              `COUNTRYCODE` VARCHAR(255) DEFAULT NULL,
+                              `OSLANGUAGE` VARCHAR(255) DEFAULT NULL,
+                              `CURTIMEZONE` VARCHAR(255) DEFAULT NULL,
+                              `LOCALE` VARCHAR(255) DEFAULT NULL,
+                              PRIMARY KEY  (`ID`,`HARDWARE_ID`)
+                            ) ENGINE=INNODB ;");
 }
 
-function plugin_init_osinstall()
+/**
+ * This function is called on removal and is used to destroy database schema for the plugin
+ */
+function extension_delete_osinstall()
 {
-$object = new plugins;
-$object -> add_cd_entry("osinstall","software");
-
-// Osinstall table creation
-
-$object -> sql_query("CREATE TABLE IF NOT EXISTS `osinstall` (
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `HARDWARE_ID` INT(11) NOT NULL,
-  `INSTDATE` VARCHAR(255) DEFAULT NULL,
-  `BUILDVER` VARCHAR(255) DEFAULT NULL,
-  `CODESET` VARCHAR(255) DEFAULT NULL,
-  `COUNTRYCODE` VARCHAR(255) DEFAULT NULL,
-  `OSLANGUAGE` VARCHAR(255) DEFAULT NULL,
-  `CURTIMEZONE` VARCHAR(255) DEFAULT NULL,
-  `LOCALE` VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY  (`ID`,`HARDWARE_ID`)
-) ENGINE=INNODB ;");
-
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("DROP TABLE `osinstall`;");
 }
 
-function plugin_delete_osinstall()
+/**
+ * This function is called on plugin upgrade
+ */
+function extension_upgrade_osinstall()
 {
-$object = new plugins;
-$object -> del_cd_entry("osinstall");
-
-$object -> sql_query("DROP TABLE `osinstall`;");
 
 }
-
-?>
